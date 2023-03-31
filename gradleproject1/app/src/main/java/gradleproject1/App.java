23f7a4adc3314131f7076ab5165a3465dfb1aa43
Sergio <prgsergiomb@gmail.com>
2023-03-31 11:26:55 +0200

    Primer commit

---
+++ gradleproject1/app/src/main/java/gradleproject1/App.java
@@ -0,0 +1,48 @@
+package gradleproject1;
+
+import static com.google.common.math.IntMath.isPrime;
+import org.apache.commons.math3.primes.Primes;
+
+public class App {
+
+    public String getGreeting() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new App().getGreeting());
+
+        int x1 = 17;
+        int x2 = 18;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("El driver funciona");
+            if (Primes.isPrime(x1)) {
+                System.out.println("El número " + x1 + " és primo");
+            } else {
+                System.out.println("El número " + x1 + " no és primo");
+            }
+            if (Primes.isPrime(x2)) {
+                System.out.println("El número " + x2 + " és primo");
+            } else {
+                System.out.println("El número " + x2 + " no és primo");
+            }
+        } catch (ClassNotFoundException e) {
+            System.out.println("Error en el driver de la BD");
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    public Boolean esPrime(float op1) {
+
+        return true;
+
+    }
+
+    public Integer proximPrim(float op1) {
+
+        return null;
+
+    }
+
+}
